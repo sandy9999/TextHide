77955c2d91def1a5c3f6c27f8f2deabe25e585d6
sandy9999 <sandy@DESKTOP-40B5H3P.localdomain>
2021-03-03 14:30:51 +0530

    Inspecting into decoder_outputs + encoder_outputs

--- transformers_hide/modeling_encoder_decoder.py
+++ transformers_hide/modeling_encoder_decoder.py
@@ -476,7 +476,7 @@ class EncoderDecoderModel(PreTrainedModel):
 
         print("Decoder outputs: " + str(decoder_outputs))
         print("Decoder + Encoder outputs: " + str(decoder_outputs + encoder_outputs))
-        print(type(decoder_outputs + encoder_outputs))
+        print((decoder_outputs + encoder_outputs)[0] == decoder_outputs)
 
         '''
         #extra stuff starts
