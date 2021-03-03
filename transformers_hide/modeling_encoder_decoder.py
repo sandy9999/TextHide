94fb12b59da5e7cdcd2288b63a679d6026b66d34
sandy9999 <sandy@DESKTOP-40B5H3P.localdomain>
2021-03-03 17:06:39 +0530

    Examining the type of decoder_output + encoder_output

--- transformers_hide/modeling_encoder_decoder.py
+++ transformers_hide/modeling_encoder_decoder.py
@@ -480,7 +480,7 @@ class EncoderDecoderModel(PreTrainedModel):
         print("Type of decoder outputs part 2: " + str(type((decoder_outputs + encoder_outputs)[1])))
         #print(type((decoder_outputs + encoder_outputs)[2]))
         #print(type((decoder_outputs + encoder_outputs)[3]))
-        print((decoder_outputs + encoder_outputs)[0] == decoder_outputs)
+        print((decoder_outputs + encoder_outputs)[:1] == decoder_outputs)
         
 
         '''
