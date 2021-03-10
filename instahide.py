027eab8e0fe2b493f1e5a328de6ae0017cfbc500
sandy9999 <sandy@DESKTOP-40B5H3P.localdomain>
2021-03-10 17:03:22 +0530

    Check target in label_to_onehot

--- instahide.py
+++ instahide.py
@@ -10,7 +10,9 @@ def cross_entropy_for_onehot(pred, target):
 
 
 def label_to_onehot(target, num_classes=2):
+    print("Target in label_to_onehot: " + str(target))
     target = torch.unsqueeze(target, 1)
+    print("Target in label_to_onehot after unsqueezing: " + str(target))
     onehot_target = torch.zeros(target.size(
         0), num_classes, device=target.device)
     onehot_target.scatter_(1, target, 1)
